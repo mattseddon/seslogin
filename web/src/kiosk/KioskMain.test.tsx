2ec933e78eef921a2487ad1bcf7ba6819d7c4e4d
Matt Seddon <37993418+mattseddon@users.noreply.github.com>
2026-07-12 09:20:19 +1000

    Use msw to mock graphql query in smoke test (#31)

--- web/src/kiosk/KioskMain.test.tsx
+++ web/src/kiosk/KioskMain.test.tsx
@@ -1,50 +1,56 @@
 import "@testing-library/jest-dom/vitest";
 import { vi, describe, it, expect } from "vitest";
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
+import { getGraphQLEndpoint } from "../lib/api";
+import { beforeAll, afterEach, afterAll } from "vitest";
+import { setupServer } from "msw/node";
+import { graphql, HttpResponse } from "msw";
 import KioskMain from "./KioskMain";
-import type { KioskSession } from "./components/KioskSessionContext";
-import * as KioskTokenSessionFetcher from "./components/KioskTokenSessionFetcher";
+
+const relayUrl = getGraphQLEndpoint();
+const relayEndpoint = graphql.link(relayUrl);
+
+const graphqlHandlers = [
+  relayEndpoint.query("KioskTokenSessionFetcherQuery", () => {
+    return HttpResponse.json({
+      data: {
+        refresh_token: "not-a-refreshed-token",
+        session: {
+          id: "mockId",
+          name: "mockName",
+          config: {},
+          location: {
+            id: "mockLocationId",
+            name: "mockLocation",
+          },
+        },
+      },
+    });
+  }),
+];
+
+const server = setupServer(...graphqlHandlers);
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
 
 describe("KioskMain", () => {
-  it("renders the main screen given a valid session", () => {
+  it("renders the main screen given a valid session", async () => {
     const settings = {
       scanAuthToken: "fun-token",
       scanAuthTokenIssuedAt: new Date().getTime(),
     };
 
-    const mockSession: KioskSession = {
-      id: "mockId",
-      name: "mockName",
-      config: {},
-      location: {
-        id: "mockLocationId",
-        name: "mockLocation",
-      },
-    };
-
     const getItemSpy = vi.spyOn(localStorage, "getItem");
     getItemSpy.mockReturnValue(JSON.stringify(settings));
 
-    const startKioskTokenSessionFetcherSpy = vi.spyOn(
-      KioskTokenSessionFetcher,
-      "default",
-    );
-    startKioskTokenSessionFetcherSpy.mockImplementation(
-      ({
-        setSession,
-      }: {
-        setSession: (session: KioskSession | null) => void;
-      }) => {
-        setSession(mockSession);
-        return () => {};
-      },
-    );
-
     render(<KioskMain />);
     expect(getItemSpy).toHaveBeenCalledOnce();
-    expect(startKioskTokenSessionFetcherSpy).toHaveBeenCalledOnce();
-    expect(
-      screen.getByText("Please enter or scan your SES ID"),
-    ).toBeInTheDocument();
+    await waitFor(() =>
+      expect(
+        screen.getByText("Please enter or scan your SES ID"),
+      ).toBeInTheDocument(),
+    );
   });
 });
